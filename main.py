cf0f4dbc332bb657448360043c14f164b6074914
Marcio Euripedes <o2junior@yahoo.com.br>
2019-10-30 15:25:39 -0300

    Inclusao de mensagem de fim das threads.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import math
 import time
 
 
+# ------------------------------ Classes Auxiliares ----------------------------- #
 class Thread(threading.Thread):
     def __init__(self, parts, step, items):
         self.items = items
@@ -17,29 +18,32 @@ class Thread(threading.Thread):
     def run(self):
         for t in range(self.begin, self.end):
             # item = self.items[t]
-            # Cálculo para simular situalção de processamento de dados
+            ''' Cálculo para simular situalção de processamento de dados '''
             # result = math.sqrt(item)
-            # Delay para simular situação de resposta de requisicao
+            ''' Delay para simular situação de resposta de requisicao '''
             time.sleep(1)
             # print("Running: {}, Thread: {}, Result: {}".format(self.items[t], self.step, math.sqrt(cont)))
 
 
+# ------------------------------------ Main ------------------------------------ #
 def main():
     items = range(200)
-    parts = 20
+    parts = 10
     thread_list = []
     for i in range(1, parts+1):
         thread = Thread(parts, i, items)
         thread_list.append(thread)
         thread.start()
-        # print('Start thread: {}'.format(i))
+        print('Start thread: {}'.format(i))
     for th in thread_list:
+        # Aguardar o fim do processamento de todas as threads
         th.join()
+    print('Threads finalizadas.')
 
 
 if __name__ == "__main__":
     for _ in Timerit(num=1, verbose=2):
-        print("=============================================================================================")
-        print("||=======================.Script para processo paralelizado em lote.=======================||")
-        print("=============================================================================================")
+        print("==========================================================================")
+        print("||==============.Script para processo paralelizado em lote.==============||")
+        print("==========================================================================")
         main()
